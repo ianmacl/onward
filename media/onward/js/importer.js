4d3dac1031d1c4df235563b215e500c081e5557e
ian_maclennan <ian.maclennan@joomla.org>
2011-02-04 21:40:17 -0500

    Added missing files, updated javascript

--- media/onward/js/importer.js
+++ media/onward/js/importer.js
@@ -14,7 +14,9 @@ var Importer = new Class({
 	
 	doBatch: function(){
 		var jsonRequest = new Request.JSON({url: this.options.url, onSuccess: function(status){
-			alert(status);
-		}}).get({'format': 'json', id: this.options.site_id, option: 'com_onward', task: 'site.batch'});
+			status.each(function(item) {
+				progressBars['progress_' + item.asset].set(Math.round(item.offset / item.total * 100));
+			});
+		}, onFailure: function() { alert('test')}}).get({'format': 'json', id: this.options.site_id, option: 'com_onward', task: 'site.batch'});
 	}
 });
