36713df1e47b80bfb0aa474b651c125989bb4a1e
Ian MacLennan <ian.maclennan@joomla.org>
2012-03-05 23:32:35 -0500

    Fixing division by zero error for javascript progress bars

--- media/onward/js/importer.js
+++ media/onward/js/importer.js
@@ -11,11 +11,15 @@ var Importer = new Class({
 	initialize: function(options){
 		this.setOptions(options);
 	},
-	
+
 	doBatch: function(){
 		var jsonRequest = new Request.JSON({url: this.options.url, onSuccess: function(status){
 			status.each(function(item) {
-				progressBars['progress_' + item.asset].set(Math.round(item.offset / item.total * 100));
+				if (item.total == 0) {
+					progressBars['progress_' + item.asset].set(100);
+				} else {
+					progressBars['progress_' + item.asset].set(Math.round(item.offset / item.total * 100));
+				}
 			});
 		}, onFailure: function() { alert('test')}}).get({'format': 'json', id: this.options.site_id, option: 'com_onward', task: 'site.batch'});
 	}
